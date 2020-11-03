78f7720d7b70406dd2c5784796ea4723a9ec901a
JiyoonChae <71802752+JiyoonChae@users.noreply.github.com>
2020-11-03 09:51:39 +0900

    create test filter

---
+++ src/main/java/TestFilter.java
@@ -0,0 +1,56 @@
+
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class TestFilter
+ */
+public class TestFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public TestFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		
+		//notice write, update, delete 관리자만 가능
+		
+		System.out.println("요청 발생");
+		System.out.println("ds 진입 전");
+		chain.doFilter(request, response);
+	
+		System.out.println("응답 발생");
+		System.out.println("ds 진입 후");
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
